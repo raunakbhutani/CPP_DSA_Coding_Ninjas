5c93a7b4cfb36b2cc4ccb903f249bef15bd25380
Raunak Bhutani <164334118+raunakbhutani@users.noreply.github.com>
2024-09-05 14:44:36 +0530

    Create remove duplicates.cpp

---
+++ Recursion 2/remove duplicates.cpp
@@ -0,0 +1,27 @@
+/*
+Problem statement
+Given a string S, remove consecutive duplicates from it recursively.
+  */
+
+void removeConsecutiveDuplicates(char *input) {
+	/* Don't write main().
+	* Don't read input, it is passed as function argument.    
+	* Change in the given string itself.
+	* No need to return or print anything
+	* Taking input and printing output is handled automatically.
+	*/
+if(input[0]=='\0')
+return;
+
+if(input[0]==input[1]){
+int i=0;
+	while(input[i]!='\0'){
+		input[i]=input[i+1];
+		i++;
+	}
+	removeConsecutiveDuplicates	(input);
+}
+  
+else
+removeConsecutiveDuplicates	(input+1);
+}
