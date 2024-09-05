646cbd4f5e8b67554584e280298e54ea2743edbe
Raunak Bhutani <164334118+raunakbhutani@users.noreply.github.com>
2024-09-06 00:23:38 +0530

    Create Time and space complexity analysis.cpp

---
+++ Time and space complexity analysis.cpp
@@ -0,0 +1,26 @@
+/*
+Problem statement
+You have been given an integer array/list(ARR) and a number 'num'. Find and return the total number of pairs in the array/list which sum to 'num'.
+
+Note:
+Given array/list can contain duplicate elements. 
+  */
+
+#include<algorithm>
+
+int pairSum(int *arr, int n, int num){
+
+int pair=0;
+  
+     int j;
+
+for(int i=0;i<n;i++){
+     for(j=i+1;j<n;j++){
+          if(arr[i]+arr[j]==num){
+               pair++;
+          }
+     }
+}
+  
+return pair;
+}
