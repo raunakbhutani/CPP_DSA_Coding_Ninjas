702849c991794b6ce9e88a73b0b0b333decbc273
Raunak Bhutani <164334118+raunakbhutani@users.noreply.github.com>
2024-12-25 14:36:04 +0530

    Create search in trie.cpp

---
+++ tries/search in trie.cpp
@@ -0,0 +1,80 @@
+/*
+Problem statement
+Implement the function Search for the Trie class.
+
+For a Trie, write the function for searching a word. Return true if the word is found successfully, otherwise return false.
+
+Note : main function is given for your reference which we are using internally to test the code and all characters are from in small case (a-z).
+  */
+
+
+class TrieNode {
+   public:
+    char data;
+    TrieNode **children;
+    bool isTerminal;
+
+    TrieNode(char data) {
+        this->data = data;
+        children = new TrieNode *[26];
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+
+class Trie {
+    TrieNode *root;
+
+   public:
+    Trie() { 
+        root = new TrieNode('\0'); 
+    }
+
+    void insertWord(TrieNode *root, string word) {
+        // Base case
+        if (word.size() == 0) {
+            root->isTerminal = true;
+            return;
+        }
+
+        // Small Calculation
+        int index = word[0] - 'a';
+        TrieNode *child;
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        } else {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        // Recursive call
+        insertWord(child, word.substr(1));
+    }
+
+    void insertWord(string word) { 
+        insertWord(root, word); 
+    }
+
+   bool helper(TrieNode *root, string word) {
+        if(word.size() == 0) {
+            return root -> isTerminal;
+        }
+        
+        int index = word[0] - 'a';
+        
+        if(root -> children[index] != NULL) {
+            return helper(root -> children[index], word.substr(1));
+        } else {
+            return false;
+        }
+        
+    }
+    
+    bool search(string word) {
+        // Write your code here
+        return helper(root, word);
+    }
+};
